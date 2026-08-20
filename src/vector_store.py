728be6a7ee6d52f2562d8d1dac004315abf7e43a
Nitesh Kumar <57477550+Nitesh-lng@users.noreply.github.com>
2026-08-20 14:06:58 +0530

    Add vector store implementation

---
+++ src/vector_store.py
@@ -0,0 +1,32 @@
+from langchain_community.vectorstores import FAISS
+from src.config import INDEX_PATH
+from langchain_huggingface import HuggingFaceEmbeddings
+from src.config import EMBEDDING_MODEL
+from langchain_community.vectorstores.utils import DistanceStrategy
+
+class VectorStore:
+
+    def __init__(self):
+        self.embeddings = HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL,
+            encode_kwargs={
+                "normalize_embeddings": True
+            },
+        )
+
+    def build(self,chunks):
+        return FAISS.from_documents(
+            chunks,
+            self.embeddings,
+            distancestrategy = DistanceStrategy.COSINE
+        )
+
+    def save(self,vector_store):
+        vector_store.save_local(INDEX_PATH)
+
+    def load(self):
+        return FAISS.load_local(
+            INDEX_PATH,
+            self.embeddings,
+            allow_dangerous_deserialization=True
+        )
